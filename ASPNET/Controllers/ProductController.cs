1cd6619e1f7618cb716668bb01ae3a332fcac16f
thatpersoninthemirror <ryan.bishop258@gmail.com>
2020-02-24 16:21:55 -0600

    updated to display products

---
+++ ASPNET/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNET.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repo;
+        public ProductController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _repo.GetAllProducts();
+
+            return View(products);
+        }
+    }
+}
---
+++ ASPNET/Models/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using Dapper;
+
+namespace ASPNET.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM Products;");
+        }
+    }
+}
